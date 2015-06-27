a7eda058334352353665ab230be28e44f3060f10
Tony Arkles <t.arkles@gmail.com>
2015-06-26 20:59:37 -0600

    Removing debugging printf

--- src/reader.cpp
+++ src/reader.cpp
@@ -8,7 +8,6 @@ UsbReader::UsbReader(libusb_device_handle* dev) {
 
 UsbReader::~UsbReader() {
 	this->run_reader = false;
-        printf("~UsbReader()\n");
 	if (this->reader.joinable()) {
 		this->reader.join();
 	}

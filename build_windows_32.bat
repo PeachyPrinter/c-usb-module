8fe476c6c23acadf2b47cb0d49b0426f38353e89
James Townley <software@peachyprinter.com>
2016-02-17 13:44:09 -0600

    Quotes

--- build_windows_32.bat
+++ build_windows_32.bat
@@ -51,7 +51,7 @@ IF NOT "%ERRORLEVEL%" == "0" (
 )
 cd _build
 
-cmake .. -G 'Visual Studio 12 2013 Win64'
+cmake .. -G "Visual Studio 12 2013 Win64"
 IF NOT "%ERRORLEVEL%" == "0" (
   ECHO FAILED executing command: cmake .. -G "Visual Studio 12 2013"
   EXIT /B 102
--- build_windows_64.bat
+++ build_windows_64.bat
@@ -52,7 +52,7 @@ IF NOT "%ERRORLEVEL%" == "0" (
 )
 cd _build
 
-cmake .. -G 'Visual Studio 12 2013 Win64'
+cmake .. -G "Visual Studio 12 2013 Win64"
 IF NOT "%ERRORLEVEL%" == "0" (
   ECHO FAILED executing command: cmake .. -G "Visual Studio 12 2013 Win64"
   EXIT /B 102
